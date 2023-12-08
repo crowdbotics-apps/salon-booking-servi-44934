766dd515f687825e27fd12f8091ae9076f8e8a30
Crowdbotics <team@crowdbotics.com>
2023-12-08 23:58:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "salon-booking-servi-44934",
+  url: "https://salon-booking-servi-44934.botics.co",
+  api: "https://salon-booking-servi-44934.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
